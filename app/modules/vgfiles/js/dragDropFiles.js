35c9109815a2a1053fd658e9a7d5836e504a39f7
Alexander Vetlugin <vetlugins@gmail.com>
2025-12-22 13:19:33 +0300

    Update dragDropFiles.js

--- app/modules/vgfiles/js/dragDropFiles.js
+++ app/modules/vgfiles/js/dragDropFiles.js
@@ -7,8 +7,70 @@ class DragDropFiles {
 	}
 
 	init() {
-		//console.log(this._element);
+
 	}
+
+	_addEventListeners() {
+		const dropZone = this._element;
+
+		// Добавляем визуальный класс при наведении
+		const handleDragOver = (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			console.log(this._element.classList.contains('drag-over'))
+			if (!this._element.classList.contains('drag-over')) {
+				this._element.classList.add('drag-over');
+			}
+		};
+
+		const handleDragLeave = (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			this._element.classList.remove('drag-over');
+		};
+
+		const handleDrop = (e) => {
+			e.preventDefault();
+			e.stopPropagation();
+			this._element.classList.remove('drag-over');
+
+			const files = e.dataTransfer.files;
+			if (files.length) {
+				this._handleDroppedFiles(files);
+			}
+		};
+
+		// Сохраняем обработчики для возможности удаления
+		this._dragHandlers = { handleDragOver, handleDragLeave, handleDrop };
+
+		this._element.addEventListener('dragover', handleDragOver);
+		this._element.addEventListener('dragleave', handleDragLeave);
+		this._element.addEventListener('drop', handleDrop);
+	}
+
+	dispose() {
+		super.dispose();
+
+		// Удаляем drag & drop обработчики
+		/*if (this._dragHandlers && this._params.dragDrop) {
+			const dropZone = this._element;
+			dropZone.removeEventListener('dragover', this._dragHandlers.handleDragOver);
+			dropZone.removeEventListener('dragleave', this._dragHandlers.handleDragLeave);
+			dropZone.removeEventListener('drop', this._dragHandlers.handleDrop);
+			this._dragHandlers = null;
+		}*/
+	}
+
+	_handleDroppedFiles(files) {
+		// Создаём временный инпут для эмуляции события change
+		const tempInput = document.createElement('input');
+		tempInput.type = 'file';
+		tempInput.files = files;
+
+		// Эмулируем change событие
+		this.change(tempInput);
+	}
+
 }
 
 export default DragDropFiles;
